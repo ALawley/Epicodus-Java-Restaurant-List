9f3300472188368cb51ab4fc80f4e25ba04542b0
Anna Lawley and Polina Nenchev <polina.mihova@yahoo.com>
2016-02-16 10:17:06 -0800

    add front-end

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -7,6 +7,34 @@ import static spark.Spark.*;
 
 public class App {
   public static void main(String[] args) {
+    staticFileLocation("/public");
+    String layout = "templates/layout.vtl";
 
+    get("/", (request, response) -> {
+      HashMap<String, Object>model =  new HashMap<String, Object>();
+      model.put("restaurants", request.session().attribute("restaurants"));
+
+      model.put("template", "templates/index.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
+
+    post("/restaurants", (request, response) -> {
+      HashMap<String, Object>model= new HashMap<String, Object>();
+
+      ArrayList<Restaurants> restaurants = request.session().attribute("restaurants");
+
+      if ( restaurants == null) {
+        restaurants = new ArrayList<Restaurants>();
+        request.session().attribute("restaurants", restaurants);
+      }
+
+      String restaurantName = request.queryParams("restaurantName");
+      Restaurants newRestaurant = new Restaurants(restaurantName);
+
+      restaurants.add(newRestaurant);
+
+      model.put("template", "templates/success.vtl");
+      return new ModelAndView(model, layout);
+    }, new VelocityTemplateEngine());
   }
 }
--- src/test/java/AppTest.java
+++ src/test/java/AppTest.java
@@ -19,18 +19,37 @@ public class AppTest extends FluentTest {
   @ClassRule
   public static ServerRule server = new ServerRule();
 
-  // @Test
-  // public void rootTest() {
-  //     goTo("http://localhost:4567/");
-  //     assertThat(pageSource()).contains("Task list");
-  // }
-  //
-  // @Test
-  // public void checkIfTaskDisplayed() {
-  //     goTo("http://localhost:4567/");
-  //     fill("#description").with("Wash the dishes");
-  //     submit(".btn");
-  //     click("a", withText("Go Back"));
-  //     assertThat(pageSource()).contains("Wash the dishes");
-  // }
+  @Test
+  public void rootTest() {
+      goTo("http://localhost:4567/");
+      assertThat(pageSource()).contains("Restaurants have been added");
+  }
+  @Test
+  public void restaurantAddedTest() {
+      goTo("http://localhost:4567/");
+      fill("#restaurantName").with("Boxer Ramen");
+      submit(".btn");
+      assertThat(pageSource()).contains("The restaurant of your choice has been added.");
+  }
+  @Test
+  public void checkIfRestaurantDisplayedTest() {
+      goTo("http://localhost:4567/");
+      fill("#restaurantName").with("Boxer Ramen");
+      submit(".btn");
+      click("a", withText("Go Back"));
+      assertThat(pageSource()).contains("Boxer Ramen");
+  }
+  @Test
+  public void checkIfMultipleRestaurantAreDisplayedTest() {
+      goTo("http://localhost:4567/");
+      fill("#restaurantName").with("Boxer Ramen");
+      submit(".btn");
+      click("a", withText("Go Back"));
+      fill("#restaurantName").with("The Nines");
+      submit(".btn");
+      click("a", withText("Go Back"));
+      assertThat(pageSource()).contains("The Nines");
+      assertThat(pageSource()).contains("Boxer Ramen");
+  }
+
 }
